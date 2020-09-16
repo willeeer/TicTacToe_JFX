6878f8c8a8362356063db195ce0f8aff0073b971
Willeeer <willersabino@gmail.com>
2020-09-16 19:19:56 -0300

    Commit inicial

---
+++ src/sample/DadosJogadores.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class DadosJogadores {
+
+    private String nomeJogadorUm;
+    private String nomeJogadorDois;
+
+    public String getNomeJogadorUm() {
+        return nomeJogadorUm;
+    }
+
+    public void setNomeJogadorUm(String nomeJogadorUm) {
+        this.nomeJogadorUm = nomeJogadorUm;
+    }
+
+    public String getNomeJogadorDois() {
+        return nomeJogadorDois;
+    }
+
+    public void setNomeJogadorDois(String nomeJogadorDois) {
+        this.nomeJogadorDois = nomeJogadorDois;
+    }
+}
